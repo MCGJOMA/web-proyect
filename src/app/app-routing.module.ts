6af0f2d7f620eb38558c8437146fb658122534bc
Jose <josemjdl565@gmail.com>
2022-10-17 22:53:00 -0600

    Commit1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { RegistrarUsuarioComponent } from './Components/registrar-usuario/registrar-usuario.component';
+import { VerificarCorreoComponent } from './Components/verificar-correo/verificar-correo.component';
+import { RecuperarPasswordComponent } from './Components/recuperar-password/recuperar-password.component';
+import { DashboardsComponent } from './Components/dashboards/dashboards.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'registrar-usuario', component: RegistrarUsuarioComponent },
+  { path: 'verificar-correo', component: VerificarCorreoComponent },
+  { path: 'recuperar-password', component: RecuperarPasswordComponent },
+  { path: 'dashboard', component: DashboardsComponent },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { DashboardsComponent } from './Components/dashboards/dashboards.component';
+import { RegistrarUsuarioComponent } from './Components/registrar-usuario/registrar-usuario.component';
+import { VerificarCorreoComponent } from './Components/verificar-correo/verificar-correo.component';
+import { RecuperarCpasswordComponent } from './Components/recuperar-password/recuperar-cpassword.component';
+import { SpinnerComponent } from './Shared/spinner/spinner.component';
+import { RecuperarPasswordComponent } from './Components/recuperar-password/recuperar-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardsComponent,
+    RegistrarUsuarioComponent,
+    VerificarCorreoComponent,
+    RecuperarCpasswordComponent,
+    SpinnerComponent,
+    RecuperarPasswordComponent
   ],
   imports: [
     BrowserModule,

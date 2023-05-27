3fa0f948b5efd3115e80fe54ead3234bdd8aed4a
Jose Juarez <josemjdl565@gmail.com>
2023-05-27 16:40:53 -0600

    Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,5 @@
+import { environment as common } from './environment';
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: '...',
-    authDomain: 'project.firebaseapp.com',
-    databaseURL: 'https://project.firebaseio.com',
-    projectId: 'project',
-    storageBucket: 'project.appspot.com',
-    messagingSenderId: '...',
-  },
+  ...common,
+  production: true
 };

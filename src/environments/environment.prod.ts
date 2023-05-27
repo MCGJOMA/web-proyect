b34c09ae109162ab69360067013da80122e4fa72
Jose Juarez <josemjdl565@gmail.com>
2023-05-27 16:42:29 -0600

    fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
-import { environment as common } from './environment';
 export const environment = {
-  ...common,
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyDWvwOuXsW0EU8XzFAAlJnejW6tcXyZEXM",
+    authDomain: "fir-web-62961.firebaseapp.com",
+    projectId: "fir-web-62961",
+    storageBucket: "fir-web-62961.appspot.com",
+    messagingSenderId: "552482095272",
+    appId: "1:552482095272:web:0f56ee7c232114f32a429d",
+}
 };
